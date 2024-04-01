e199c091710965b08ea79d6f6c9d8eabc068a8ff
Gary Bright <garybright@rocketmail.com>
2024-04-01 15:50:28 -0400

    added inputs and lists for prompt

--- index.js
+++ index.js
@@ -4,8 +4,29 @@ const fs = require("fs");
 
 inquirer
 .prompt([{
-    type: 'list'
-}])
+    type: 'input',
+    name: 'text',
+    message: 'Enter up to three characters:'
+},
+{
+    type: 'input',
+    name: 'textColor',
+    message: 'Text Color: Enter a color keyword or a hexadecimal number'
+},
+{
+    type: 'list',
+    name: 'shape',
+    message: 'Please choose a shape:',
+    choices: ['Circle', 'Triangle', 'Square']
+},
+{
+    type: 'list',
+    name: 'shapeColor',
+    message: 'Shape Color: Enter a color keyword or a hexadecimal number'
+}
+
+])
+
 
 //DATA
 
